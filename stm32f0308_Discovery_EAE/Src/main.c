c32e772e4bae0523f94af80a039aefa8402cb4e5
Ugur Kebir <ugurkebir_91@yahoo.com.tr>
2020-11-03 21:58:59 +0300

    Update main.c
    test amacli yapildi

--- stm32f0308_Discovery_EAE/Src/main.c
+++ stm32f0308_Discovery_EAE/Src/main.c
@@ -105,8 +105,7 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
-
-	HAL_TIM_Base_Start_IT(&htim1);
+  HAL_TIM_Base_Start_IT(&htim1);
   /* USER CODE END 2 */
 
   /* Infinite loop */
